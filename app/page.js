51e4a428c32b9fefd0b2c7e1a18e99f5129bc5c0
Jon Shek <jon.shek@amplitude.com>
2024-06-17 22:26:03 -0700

    session replays working

--- app/page.js
+++ app/page.js
@@ -3,9 +3,11 @@
 import { useEffect } from "react";
 import Image from "next/image";
 import { logEvent } from "../lib/amplitude"
+import Amplitude from "../lib/amplitude"
 
 export default function Home() {
   useEffect(() => {
+
     const handleClick = (event) => {
       const card = event.currentTarget;
       const title = card.querySelector('.card-title').textContent;
@@ -26,6 +28,8 @@ export default function Home() {
   }, []);
 
   return (
+    <>
+    <Amplitude />
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
@@ -62,7 +66,7 @@ export default function Home() {
           priority
         />
       </div>
-
+      
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -133,5 +137,6 @@ export default function Home() {
         </a>
       </div>
     </main>
+  </>
   );
 }
